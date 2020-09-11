1b44706b687957e67b3aa2fdcda2f61faed44068
dalhy <49164142+dalhy@users.noreply.github.com>
2020-09-11 12:44:59 -0300

    Create mention_handler.py

---
+++ modulos/events/mention_handler.py
@@ -0,0 +1,16 @@
+from discord.ext import commands
+
+import discord
+
+class on_mention(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+         if self.client.user.mention == message.content:
+            await message.channel.send(f" Olá {message.author.mention}, deseja saber quais são meus comandos? Você pode vê-los digitando ``pex ajuda``!")
+
+def setup(client):
+    client.add_cog(on_mention(client))
