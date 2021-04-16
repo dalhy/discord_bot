f59261b3be2b316683f047327b836c17f6afa102
dalhy <49164142+dalhy@users.noreply.github.com>
2021-04-16 11:40:01 -0300

    Update on_ready.py

--- modulos/events/on_ready.py
+++ modulos/events/on_ready.py
@@ -11,11 +11,7 @@ class ready(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print(f"[OK] - {self.client.user.name} ({self.client.user.id} - Status: Online)")
-        while True:
-            await self.client.change_presence(status=discord.Status.dnd, activity=discord.Activity(name="Pex Beta", type=3))
-            await asyncio.sleep(15)
-            await self.client.change_presence(activity=discord.Activity(name="muita diversão. 😊", type=1))
-            await asyncio.sleep(15)
+        await self.client.change_presence(activity=discord.Activity(name="😊", type=1))
 
 def setup(client):
     client.add_cog(ready(client))
